4c282e111ee3f5f10d1948ef4e915cf0395993b4
doer <499361328@qq.com>
2019-05-08 13:17:21 +0800

    创建工程，配置环境， 2 Create Your First Data-bound Component

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-app',
+  template: '<app-events-list></app-events-list>'
+})
+export class EventsAppComponent {
+  title = 'app';
+}
