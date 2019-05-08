c22cfb9d0167560c848d85a3a2296b300409143b
doer <499361328@qq.com>
2019-05-08 19:32:06 +0800

    Angular Fundamental (4 Exploring the Angular Template Syntax) : 7) Hiding Content with the [Hidden] Binding, 8) Hiding and Showing Content with ngSwitch, 9) Styling Components with ngStyle

--- ng-fundamentals/src/app/events/events-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/events-thumbnail.component.ts
@@ -6,18 +6,25 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
-      <div *ngIf="event?.location">
+      <div [hidden]="!event?.location">
         <span>Localtion: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
-      <div *ngIf="event?.onlineUrl">
+      <div [hidden]="!event?.onlineUrl">
         Online URL: {{event?.onlineUrl}}
       </div>
     </div>
   `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold }
     .thumbnail { min-height: 210px }
     .pad-left { margin-left: 10px; }
     .well div { color: red; }
@@ -26,4 +33,12 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 export class EventsThumbnailComponent {
   @Input() event: any;
+  getStartTimeClass() {
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return {green: isEarlyStart, bold: isEarlyStart};
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
+  }
 }
