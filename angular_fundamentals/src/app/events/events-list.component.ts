5e3cbe69d6d1e13a9c5a5bdb53fa2f8b57d2f8ed
doer <499361328@qq.com>
2019-05-09 23:41:28 +0800

    Angular Fundamental (7 Collecting Data with Angular Forms and Validation) : 2) ~ 4)

--- angular_fundamentals/src/app/events/events-list.component.ts
+++ angular_fundamentals/src/app/events/events-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared/index';
 
 
 @Component({
@@ -25,7 +26,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class EventsListComponent implements OnInit {
-  events: any;
+  events: IEvent[];
   constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) {}
   ngOnInit() {
     // this.eventService.getEvents().subscribe(events => { this.events = events; });
